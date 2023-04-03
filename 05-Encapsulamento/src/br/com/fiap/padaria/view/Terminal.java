5000d9ef6755bafac52347ebefc241580f3bbee1
rm550181 <rm550181@fiap.com.br>
2023-04-03 12:44:27 -0300

    encapsulamento

---
+++ 05-Encapsulamento/src/br/com/fiap/padaria/view/Terminal.java
@@ -0,0 +1,21 @@
+package br.com.fiap.padaria.view;
+
+import br.com.fiap.padaria.model.Produto;
+
+public class Terminal {
+
+	//Criar o método: main + CTRL + espaco
+	public static void main(String[] args) {
+		//Instanciar um produto
+		Produto p = new Produto();
+		
+		//Ajustar para utilizar os métodos set
+		p.setNome("CD");
+		p.setId(5);
+		
+		
+		//Exibir os atributos do produto
+		
+		
+	}
+}
